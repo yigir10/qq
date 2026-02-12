70e6459cdbb36bb28796d310f54a6ad605216ccf
Samsung-08 <ivarol788@gmail.com>
2026-02-12 19:37:43 +0300

    1.0

---
+++ src/Game.java
@@ -0,0 +1,15 @@
+public class Game {
+
+    public static void main(String[] args) {
+
+        int step = 0;
+        int personX;
+        int personY;
+        int personLive = 5;
+        int sizeBoard = 5;
+        personX = 1 + sizeBoard / 2;
+        personY = 1 + sizeBoard / 2;
+        System.out.println("Привет! Ты готов начать играть в игру? (Напиши: ДА или НЕТ)");
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.ls.LSInput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        System.out.println("Hello, " + n);
+    }
+}
