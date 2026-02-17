9b28bf146f8e395e310dd33afd2fe9037d4d67aa
Samsung-08 <ivarol788@gmail.com>
2026-02-17 17:59:10 +0300

    1.0

---
+++ src/ElseIf.java
@@ -0,0 +1,10 @@
+public class ElseIf {
+    public static void main(String[] args) {
+
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+        }
+
+
+    }
+}
--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
@@ -6,11 +7,16 @@ public class Game {
         int personX;
         int personY;
         int personLive = 3;
-        int sizeBoard = 3;
-        personX = sizeBoard / 2;
-        personY = sizeBoard / 2;
+        int sizeBoard = 5;
+        int difficultGame;
+        Random random = new Random();
+        int castleX = random.nextInt(sizeBoard) + 1;
+        int castleY = 1;
+        personX = sizeBoard / 2 + 1;
+        personY = sizeBoard / 2 + 1;
         String person = "\uD83D\uDC76\uD83C\uDFFF";
         String monster = "\uD83D\uDC79";
+        String castle = "\uD83C\uDFF0";
         String gamingField = "+ —— + —— + —— +\n"
                 + "| "+ person + " |    |    |\n"
                 + "+ —— + —— + —— +\n"
@@ -18,7 +24,9 @@ public class Game {
                 + "+ —— + —— + —— +\n"
                 + "|    |    |    |\n"
                 + "+ —— + —— + —— +";
-        int difficultGame;
+        String leftBlock = " | ";
+        String rightBlock = " |";
+        String wall = " + —— + —— + —— + —— + —— + ";
 
         System.out.println("Привет! Ты готов начать играть в игру? Напиши: Да :)");
         Scanner scanner = new Scanner(System.in);
@@ -36,25 +44,42 @@ public class Game {
                 System.out.println("Некорректный уровень сложности.");
                 System.exit(0);
             }
+            while (personLive > 0 && !(castleX == personX && castleY == personY)) {
 
-            System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;");
-            System.out.println("Координаты персонажа - (x: " + personX + ", y: " + personY + "))");
+                for (int yCount = 1; yCount <= sizeBoard; yCount++) {
+                    System.out.println(wall);
+                    for (int xCount = 1; xCount <= sizeBoard; xCount++) {
+                        System.out.print(leftBlock);
+                        if (personY == yCount && personX == xCount) {
+                            System.out.print(person);
+                        } else if (castleX == xCount && castleY == yCount) {
+                            System.out.print(castle);
+                        } else {
+                            System.out.print("  ");
+                        }
+                    }
+                    System.out.println(rightBlock);
+                }
+                System.out.println(wall);
 
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
+                System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;");
+                System.out.println("Координаты персонажа - (x: " + personX + ", y: " + personY + "))");
 
-            if (x != personX && y != personY) {
-                System.out.println("Неккоректный ход");
-            }
-            else if (Math.abs(x - personX) == 1 || Math.abs(y - personY) == 1) {
-                personX = x;
-                personY = y;
-                step += 1;
-                System.out.println("Ход корректный; Новые координаты: " +
-                        personX + ", " + personY + "\nХод номер: " + step);
-            }
-            else {
-                System.out.println("Координаты не изменены");
+
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+
+                if (x != personX && y != personY) {
+                    System.out.println("Неккоректный ход");
+                } else if (Math.abs(x - personX) == 1 || Math.abs(y - personY) == 1) {
+                    personX = x;
+                    personY = y;
+                    step += 1;
+                    System.out.println("Ход корректный\nНовые координаты: " +
+                            personX + ", " + personY + "\nХод номер: " + step);
+                } else {
+                    System.out.println("Координаты не изменены");
+                }
             }
         }
         else {
