01ce65bb75f0dc79e2196d62a69da667aa574562
Samsung-08 <ivarol788@gmail.com>
2026-02-17 18:07:47 +0300

    1.0

--- src/ElseIf.java
+++ src/ElseIf.java
@@ -1,8 +1,11 @@
 public class ElseIf {
     public static void main(String[] args) {
 
-        for (int i = 5; i > 0; i--) {
-            System.out.println(i);
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print((j + 1) + " ");
+            }
+            System.out.println("");
         }
 
 
