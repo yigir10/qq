23fd2ff9d3e76b9f6e83d81437492495b0f8349a
yigir10 <yigir111@gmail.com>
2026-02-17 14:52:47 +0300

    1.2

--- src/Game.java
+++ src/Game.java
@@ -6,22 +6,59 @@ public class Game {
         int personX;
         int personY;
         int personLive = 3;
-        int sizeBoard = 5;
+        int sizeBoard = 3;
         personX = sizeBoard / 2;
         personY = sizeBoard / 2;
-        personX = 1 + sizeBoard / 2;
-        personY = 1 + sizeBoard / 2;
-        String person = "Я";
-        String monster = "М";
+        String person = "\uD83D\uDC76\uD83C\uDFFF";
+        String monster = "\uD83D\uDC79";
         String gamingField = "+ —— + —— + —— +\n"
-                + "|    |    |    |\n"
+                + "| "+ person + " |    |    |\n"
                 + "+ —— + —— + —— +\n"
                 + "|    | " + monster + " |    |\n"
                 + "+ —— + —— + —— +\n"
-                + "| " + person + " |    |    |\n"
+                + "|    |    |    |\n"
                 + "+ —— + —— + —— +";
-        Scanner sc = new Scanner(System.in);
-        double answer = sc.nextDouble();
+        int difficultGame;
+
+        System.out.println("Привет! Ты готов начать играть в игру? Напиши: Да :)");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+
+        if (answer.equalsIgnoreCase("ДА")) {
+            System.out.println("Начинаем играть");
+
+            System.out.println("Выбери сложность игры(от 1 до 5):");
+            difficultGame = scanner.nextInt();
+            if (difficultGame > 0 && difficultGame < 6) {
+                System.out.println("Выбранная сложность:\t" + difficultGame);
+            }
+            else {
+                System.out.println("Некорректный уровень сложности ");
+            }
+
+            System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;");
+            System.out.println("Координаты персонажа - (x: " + personX + ", y: " + personY + "))");
+
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            if (x != personX && y != personY) {
+                System.out.println("Неккоректный ход");
+            }
+            else if (Math.abs(x - personX) == 1 || Math.abs(y - personY) == 1) {
+                personX = x;
+                personY = y;
+                step += 1;
+                System.out.println("Ход корректный; Новые координаты: " +
+                        personX + ", " + personY + "\nХод номер: " + step);
+            }
+             {
+                System.out.println("Координаты не изменены");
+            }
+        }
+        else {
+            System.out.println("Почему ты не захотел со мной играть(");
+            System.out.println("Приходи ещё!");
+        }
     }
-}
-}
+    }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String n = sc.nextLine();
-        System.out.println("Hello, " + n);
     }
 }
