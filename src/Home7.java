4d5ba84781b58463d3e7c1929ec2285f095d03b4
yigir10 <yigir111@gmail.com>
2026-02-19 16:39:50 +0300

    1.0

---
+++ src/Home7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Home7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("b - a = " + (b - a));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+    }
+}
