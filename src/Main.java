6181a6a4642327c9d928a9764cbc935d0be19b79
leledame <leledame@ya.ru>
2026-02-26 19:58:56 +0300

    1.0

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-import org.w3c.dom.ls.LSInput;
-
-import java.sql.SQLOutput;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+
+        int sizeBoard = 5;
+        Person person = new Person(sizeBoard);
+        Random random = new Random();
+        int n = random.nextInt(sizeBoard);
+        person.x = n == 0 ? 1 : n;
+        person.y = sizeBoard;
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,23 @@
+import java.util.Random;
+
 public class Person {
     int x;
     int y;
     String person = "\uD83D\uDC76\uD83C\uDFFF";
     int personLive = 3;
-
+    void move(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public boolean isMoveCorrect(int x, int y) {
+        return (this.x == x && Math.abs(this.y - y) == 1 || this.y == y && Math.abs(this.x - x) == 1) && (this.x < 6 && this.x > 0) && (this.y < 6 && this.y > 0);
+    }
+    public void reducingLives(int personLive) {
+        personLive--;
+    }
+    Person(int sizeBoard) {
+        Random random = new Random();
+        y = sizeBoard;
+        int x = random.nextInt(sizeBoard) + 1;
+    }
 }
